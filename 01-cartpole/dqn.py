32051532e061087c7cf186e215204b9a6b3dd44e
Ubuntu <ubuntu@cchyun.novalocal>
2019-06-06 13:20:48 +0900

    모델 수정

--- 01-cartpole/dqn.py
+++ 01-cartpole/dqn.py
@@ -9,34 +9,28 @@ import gym
 
 import torch
 
-from model import CartpoleQ, CartpoleConfig
+from model import ValueNet, Config
 
 
 # 참고: https://github.com/rlcode/reinforcement-learning-kr/blob/master/2-cartpole/1-dqn/cartpole_dqn.py
 
 
-"""
-Deep Q Network Agent
-"""
+# 카트폴 예제에서의 DQN 에이전트
 class DQNAgent:
-    # 초기화
     def __init__(self, config):
         self.config = config
         self.epsilon = config.epsilon
+        self.losses = []
 
         # replay memory
-        self.replay_memory = deque(maxlen=config.n_replay_memory)
+        self.replay_memory = deque(maxlen=self.config.n_replay_memory)
 
-        # train model, target model 생성
-        self.model = CartpoleQ(config.n_state, config.n_action)
-        self.model.to(self.config.device)
-        self.target = CartpoleQ(config.n_state, config.n_action)
-        self.target.to(self.config.device)
-        # MSE loss 및 optimizer
-        self.criterion = torch.nn.MSELoss()
-        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=config.learning_rate)
-    
-    # 상태에 대한 Action 에측 (epsilon 확률로 탐험:exploration)
+        # 가치신경망 생성
+        self.model = ValueNet(self.config.n_state, self.config.n_action)
+        self.model.to(device)
+        self.model_optimizer = torch.optim.Adam(self.model.parameters(), lr=self.config.learning_rate)
+
+    # 정책신경망의 출력을 받아 확률적으로 행동을 선택
     def get_action(self, state):
         if np.random.rand() <= self.epsilon:
             return random.randrange(self.config.n_action)
@@ -44,187 +38,120 @@ class DQNAgent:
             state = torch.tensor(state, dtype=torch.float).to(self.config.device)
             output = self.model(state)
             return output.argmax().item()
-    
-    # 리플레이 메모리에 St, At, Rt+1, St+1, Done 추가
+        
+    # 히스토리 추가
     def append_replay(self, state, action, reward, next_state, done):
         self.replay_memory.append((state, action, reward, next_state, done))
-    
-    # 리플레이 메모리가 일정 수 이상 수집된 후 학습이 가능 함
-    def is_train(self):
-        return True if self.config.n_train_start < len(self.replay_memory) else False
-    
-    # 학습
-    def train(self):
+
+    # 각 타임스텝마다 정책신경망과 가치신경망을 업데이트
+    def train_model(self):
         # 학습이 계속 될 수 록 탐험 학률을 줄여 줌
         if self.epsilon > self.config.epsilon_min:
             self.epsilon *= self.config.epsilon_decay
-        
-        # 리플레이 메모리에서 배치사이즈 만큼 랜덤 셈플링
-        mini_batch = random.sample(self.replay_memory, self.config.n_batch)
 
-        # 랜덤 샘플링 된 데이터를 배열 형태로 정렬
-        mini_batch = np.array(mini_batch)
-        states = np.vstack(mini_batch[:, 0])
-        actions = list(mini_batch[:, 1])
-        rewards = list(mini_batch[:, 2])
-        next_states = list(mini_batch[:, 3])
-        dones = list(mini_batch[:, 4])
+        # 히스토리를 배열 형태로 정렬
+        replay_memory = np.array(random.sample(self.replay_memory, self.config.n_batch))
+        states = np.vstack(replay_memory[:, 0])
+        actions = list(replay_memory[:, 1])
+        rewards = list(replay_memory[:, 2])
+        next_states = list(replay_memory[:, 3])
+        dones = list(replay_memory[:, 4])
 
-        states = torch.tensor(states, dtype=torch.float).to(self.config.device)
-        next_states = torch.tensor(next_states, dtype=torch.float).to(self.config.device)
+        states = torch.tensor(states, dtype=torch.float).to(device)
+        next_states = torch.tensor(next_states, dtype=torch.float).to(device)
+    
+        targets = self.model(states).detach().cpu().numpy()
+        next_values = self.model(next_states).detach().cpu().numpy()
 
-        values = self.model(states) ## 정답
-        target = np.copy(values.detach().cpu().numpy()) ## Vt
-        next_values = self.target(next_states).detach().cpu().numpy() ## Vt+1
-
-        for i in range(len(target)):
+        for i in range(len(targets)):
             if dones[i]:
-                target[i][actions[i]] = rewards[i] # Vt = Rt+1
+                targets[i][actions[i]] = rewards[i] # Vt = Rt+1
             else:
-                target[i][actions[i]] = rewards[i] + self.config.discount_factor * (np.amax(next_values[i])) # Vt = Rt+1 + rVt+1
-        target = torch.tensor(target, dtype=torch.float).to(self.config.device)
-        
-        # 학습
-        self.optimizer.zero_grad()
-        loss = self.criterion(values, target)
-        loss.backward()
-        self.optimizer.step()
+                targets[i][actions[i]] = rewards[i] + self.config.discount_factor * (np.amax(next_values[i])) # Vt = Rt+1 + rVt+1
+
+        targets = torch.tensor(targets, dtype=torch.float).to(device)
+        ValueNet_loss = self.train_ValueNet(states, targets)
+
+        self.losses.append(ValueNet_loss)
     
-    # model의 weight를 target에 저장
-    def update_target(self):
-        self.target.load_state_dict(self.model.state_dict())
-    
+    # 가치신경망을 업데이트하는 함수
+    def train_ValueNet(self, state, target):
+        value = self.model(state)
+        ValueNet_loss = torch.mean(torch.pow(target - value, 2))
+
+        self.model_optimizer.zero_grad()
+        ValueNet_loss.backward()
+        self.model_optimizer.step()
+
+        return ValueNet_loss.item()
+
     # model의 weight를 파일로 저장
-    def save(self, path):
-        torch.save(self.model.state_dict(), path)
+    def save(self):
+        torch.save(self.model.state_dict(), self.config.save_file)
     
-    # 파일로 부터 model의 weight를 읽어 옴, target에도 동일하게 적용
-    def load(self, path):
-        self.model.load_state_dict(torch.load(path))
-        self.update_target()
+    # 파일로 부터 model의 weight를 읽어 옴
+    def load(self):
+        self.model.load_state_dict(torch.load(self.config.save_file))
+    
+    # GPU 메모리 반납
+    def close(self):
+        del self.model
 
 
-def train(config):
-    # agent 생성
+def train(env, config):
+    # DQN 에이전트 생성
     agent = DQNAgent(config)
 
-    # 그래프 출력용 데이터
-    epochs = []
-    scores = []
+    scores, episodes = [], []
 
-    for epoch in range(config.n_epoch):
+    for e in range(config.n_epoch):
         done = False
         score = 0
-
-        # St: 초기 state 조회
         state = env.reset()
         state = np.reshape(state, [1, config.n_state])
 
         while not done:
             if config.render:
                 env.render()
-            
-            # At: St 대한 action 예측
+
             action = agent.get_action(state)
-            # St+1, Rt+1: action 수행
             next_state, reward, done, info = env.step(action)
             next_state = np.reshape(next_state, [1, config.n_state])
             # 에피소드가 중간에 끝나면 -100 보상
-            reward = reward if not done or score == 499 else -100
+            reward = reward if not done or score == 499 else config.fail_reward
 
-            # St, At, Rt+1, St+1, Done: 리플레이 메모리 저장
+            # 리플레이 메모리에 step 저장
             agent.append_replay(state, action, reward, next_state, done)
+            if config.n_train_start < len(agent.replay_memory):
+                agent.train_model()
 
-            if reward != -100:
+            if 0 < reward:
                 score += reward
             state = next_state
 
-            # 리플레이 메모리에 일정량 이상의 데이터가 쌓이면 매 스텝마다 학습
-            if agent.is_train():
-                agent.train()
-            
             if done:
-                print("epoch: %d, score: %d" % (epoch + 1, score))
-                agent.update_target()
-
-                epochs.append(epoch + 1)
+                # 에피소드마다 학습 결과 출력
+                losses = np.array(agent.losses)
+                agent.losses.clear()
+                ValueNet_loss = 0 if len(losses) == 0 else np.sum(losses) / len(losses)
+                print("episode: %4d,    score: %3d,    ValueNet_loss: %3.2f" % (e, score, ValueNet_loss))
                 scores.append(score)
+                episodes.append(e)
 
-                # 이전 5개 에피소드의 점수 평균이 500이면 학습 중단
-                if np.mean(scores[-min(5, len(scores)):]) > 499:
-                    agent.save(config.save_file)
-                    sys.exit()
-    # env 종료
-    env.close()
-
-    # 학습 스코어 화면 출력
-    plt.plot(epochs, scores, label='score')
-    plt.show()
-
-
-def run(config):
-   # 환경
-    config.n_epoch = 10
-    config.render = True
-    config.epsilon = 0
-
-    # agent 생성
-    agent = DQNAgent(config)
-    agent.load(config.save_file)
-
-    done = False
-    score = 0
-
-    # St: 초기 state 조회
-    state = env.reset()
-    state = np.reshape(state, [1, config.n_state])
-
-    # 그래프 출력용 데이터
-    epochs = []
-    scores = []
-
-    for epoch in range(config.n_epoch):
-        done = False
-        score = 0
-
-        # St: 초기 state 조회
-        state = env.reset()
-        state = np.reshape(state, [1, config.n_state])
-
-        while not done:
-            if config.render:
-                env.render()
-            
-            # At: St 대한 action 예측
-            action = agent.get_action(state)
-            # St+1, Rt+1: action 수행
-            next_state, reward, done, info = env.step(action)
-            next_state = np.reshape(next_state, [1, config.n_state])
-
-            score += reward
-            state = next_state
-            
-            if done:
-                print("epoch: %d, score: %d" % (epoch + 1, score))
-
-                epochs.append(epoch + 1)
-                scores.append(score)
-    # env 종료
-    env.close()
-
-    # 학습 스코어 화면 출력
-    plt.plot(epochs, scores, label='score')
-    plt.show()
+                # 이전 500점이 n_success 이상 이면 학습 중단
+                if np.mean(scores[-min(config.n_success, len(scores)):]) >= 500:
+                    agent.save()
+                    agent.close()
+                    return True, e
+    agent.close()
+    return False, 0
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('mode', choices=['train', 'run'], default='run', nargs='?')
-    args = parser.parse_args()
-
+    # 데어처 저장 폴더
     if not os.path.exists("save"):
         os.makedirs("save")
-    
+ 
     # cuda or cpu
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -237,24 +164,27 @@ if __name__ == "__main__":
     # env 초기화
     env = gym.make('CartPole-v1')
 
-    config = CartpoleConfig({
-        "device": device,
-        "n_epoch": 3000,
-        "n_state": env.observation_space.shape[0],
-        "n_action": env.action_space.n,
-        "n_batch": 64,
-        "n_replay_memory": 2000,
-        "n_train_start": 1000,
-        "learning_rate": 0.01,
-        "discount_factor": 0.99,
-        "epsilon": 1.0,
-        "epsilon_decay": 0.99,
-        "epsilon_min": 0.01,
-        "render": False,
-        "save_file": "save/dqn.torch",
+    config = Config({
+        "device": device, # cpu 또는 gpu 사용
+        "n_epoch": 1000, # 게임 에피소드 수
+        "n_state": env.observation_space.shape[0], # 상태 개수
+        "n_action": env.action_space.n, # 액션 개수
+        "n_batch": 64, # 학습 데이터 크기 (리플레이 메모리에서 셈플링 할 수)
+        "n_replay_memory": 2000, # 리플레이 메모리 최대 크기
+        "n_train_start": 1000, # 리플레이 메모리 취소 학습 시작 기준
+        "learning_rate": 0.02, # 학습율
+        "discount_factor": 0.99, # 감가율
+        "fail_reward": -100, # 중간에 실패할 경우 보상
+        "epsilon": 1.0, # 참험 확률
+        "epsilon_decay": 0.99, # 탐험확률 감소 율
+        "epsilon_min": 0.01, # 참험 확률 최소값
+        "render": False, # 화면 출력 여부
+        "save_file": "save/dqn.torch", # 할습 모델 저장 위치
+        "n_success": 5, # 500점을 몇번 연속하면 학습을 종료할 것인가 기준
     })
 
-    if args.mode == 'train':
-        train(config)
-    elif args.mode == 'run':
-        run(config)
+
+    success, e = train(env, config)
+    print("sussess: %r,    epoch: %3d" % (success, e))
+    
+    env.close()
--- 01-cartpole/model.py
+++ 01-cartpole/model.py
@@ -5,16 +5,36 @@ import torch.nn.functional as F
 
 
 """
-Cartpole Value (Q) Network
+(Value Network
 """
-class CartpoleQ(nn.Module):
+class ValueNet(nn.Module):
     def __init__(self, n_state, n_action):
-        super(CartpoleQ, self).__init__()
+        super(ValueNet, self).__init__()
 
-        self.layer1 = self.linear(n_state, 256)
-        self.layer2 = self.linear(256, 256)
-        self.output = self.linear(256, n_action)
+        self.layer1 = self.linear(n_state, 24)
+        self.output = self.linear(24, n_action)
     
+    def linear(self, n_input, n_output):
+        layer = nn.Linear(n_input, n_output)
+        nn.init.xavier_normal_(layer.weight)
+        return layer
+    
+    def forward(self, state):
+        output = F.relu(self.layer1(state))
+        output = self.output(output)
+        return output
+
+
+"""
+Ploicy Network
+"""
+class PolicyNet(nn.Module):
+    def __init__(self, n_state, n_action):
+        super(PolicyNet, self).__init__()
+        
+        self.layer1 = self.linear(n_state, 24)
+        self.output = self.linear(24, n_action)
+
     def linear(self, n_input, n_output):
         layer = nn.Linear(n_input, n_output)
         nn.init.xavier_uniform_(layer.weight)
@@ -22,38 +42,14 @@ class CartpoleQ(nn.Module):
     
     def forward(self, state):
         output = F.relu(self.layer1(state))
-        output = F.relu(self.layer2(output))
-        output = self.output(output)
-        return output
-
-
-"""
-Cartpole Policy Network
-"""
-class CartpoleP(nn.Module):
-    def __init__(self, n_state, n_action):
-        super(CartpoleP, self).__init__()
-        
-        self.layer1 = self.linear(n_state, 256)
-        self.layer2 = self.linear(256, 256)
-        self.output = self.linear(256, n_action)
-
-    def linear(self, n_input, n_output):
-        layer = nn.Linear(n_input, n_output)
-        # nn.init.xavier_uniform_(layer.weight)
-        return layer
-    
-    def forward(self, state):
-        output = F.relu(self.layer1(state))
-        output = F.relu(self.layer2(output))
         output = F.softmax(self.output(output), dim=1)
         return output
 
 
 """
-Cartpole Configuration
+Configuration
 """
-class CartpoleConfig(dict): 
+class Config(dict): 
     __getattr__ = dict.__getitem__
     __setattr__ = dict.__setitem__
 
